8e845016bff7a002a3f5a9cb526ec23069e7b5c0
Norbert Melzer <timmelzer@gmail.com>
2017-01-23 10:13:12 +0100

    Add test for scanning a value

--- compiler/scanner_test.go
+++ compiler/scanner_test.go
@@ -29,3 +29,26 @@ func TestScannerTokenRecognitionWithoutValue(t *testing.T) {
 		}
 	}
 }
+
+func TestScannerTokenRecognitionWithValue(t *testing.T) {
+	testData := []struct{input, expect string}{
+		{"1", "1"},
+	}
+
+	for _, data := range testData {
+		lexer := yylexer{
+			src: bufio.NewReader(strings.NewReader(data.input)),
+			buf: make([]byte, 100),
+			empty: true,
+			current: 0,
+		}
+
+		lval := yySymType{}
+
+		(&lexer).Lex(&lval)
+
+		if lval.string != data.expect {
+			t.Errorf("Got %#v, expected %#v.\n", lval.string, data.expect)
+		}
+	}
+}
